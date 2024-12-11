6b47de0c276775a7f85177bbce0dc7ee16a543fa
Peter Creasey <peter@mercuna.com>
2024-12-11 16:55:59 +0000

    Direct Pearson correlation. Drop slowly converging numerics

--- test_moments.py
+++ test_moments.py
@@ -27,14 +27,12 @@ print(tmtp.shape)
 m2 = (a**1.5 - (a+2)*np.sqrt(1-a) + 2)/(3)
 print('<t-,t+> %.7f numerically %.7f'%(m2, np.inner(np.transpose(tp), tm).mean()/n), tmtp.mean(), (av_tp_given_tm*tm).mean())
 
-print('sin integral')
-ds = a / (n*140)
-s = (0.5 + np.arange(n*140)) * ds
+# sin integral
+ds = a / (n)
+s = (0.5 + np.arange(n)) * ds
 v = (1/np.pi)*(np.sqrt(s)*np.arcsin(np.sqrt((1-a)/(1-s))))
-print(v.sum()*ds, (np.sqrt(1-a)+0.5*a*np.sqrt(1-a)+a**1.5-1)/3)
-print('scalar integral==<t-^2> (this converges slowly!)')
-v = (1/np.pi)*(s**1.5 / (1-s))*np.sqrt((1-a)/(a-s))
-print(v.sum()*ds, 1 - np.sqrt(1-a) * ((a+2)/2), (tm*tm).mean())
+print('sin integral', v.sum()*ds, (np.sqrt(1-a)+0.5*a*np.sqrt(1-a)+a**1.5-1)/3)
+print('scalar integral==<t-^2>', 1 - np.sqrt(1-a) * ((a+2)/2), (tm*tm).mean())
 
 var_tm = 0.5*np.sqrt(1-a)*m1*m1
 print('Variance <(t- - <t->)^2>', ((tm-m1)*(tm-m1)).mean(), var_tm)
@@ -45,5 +43,5 @@ covar = m2 - m1*tp_m1 # TODO simplify
 print('Covariance <(t+ - <t+>)(t- - <t->)>', covar, ((av_tp_given_tm - tp_m1)*(tm-m1)).mean())
 
 print('Pearson correlation coefficient')
-correl = (m2 - m1*tp_m1)/(0.5 *m1*(1-np.sqrt(a))*np.sqrt(np.sqrt((1-a)*a)))
+correl = (2/3)*(1-a**0.5 - (1-a)**0.5)/((a*(1-a))**0.25)
 print('Correl <(t+ - <t+>)(t- - <t->)>/sqrt(<(t+ - <t+>)^2><(t- - <t->)^2>', correl, ((av_tp_given_tm - tp_m1)*(tm-m1)).mean()/np.sqrt(((tm-m1)*(tm-m1)).mean() *np.square(tp-tp_m1).mean()))
