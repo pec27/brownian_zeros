70d780c9df6b97f0c5bce071d6009b813b384677
Peter Creasey <p.e.creasey.00@gmail.com>
2025-01-02 11:40:36 +0000

    Simplification: switched first and last zeros for simpler sampling formula

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ Example code for recursively simulating the zeros of a standary one-dimensional 
 
 ![Finding the last zero crossing before the midpoint, and the first crossing after](./figs/illustration.png)
 
-A sampling formula for the last zero crossing before $1/2$, i.e $W_{\tau_{\textrm{-}}}=0$ with $0 < \tau_{\textrm{-}} < 1/2$ can be found as (derivation [pdf](./brownian_zeros.pdf)),
+A sampling formula for the first zero crossing after $1/2$, i.e $W_{\tau_{\textrm{+}}}=0$ with $1/2 < \tau_{\textrm{+}} < 1$ can be found as (derivation [pdf](./brownian_zeros.pdf)),
 ```math
-\tau_{\textrm{-}} = \frac{\sin^2 \left( \frac{\pi}{2} U_1 \right)}{1+\sin^2 \left( \frac{\pi}{2} U_1 \right)}
+\tau_{\textrm{+}} = \frac{1}{1+\sin^2 \left( \frac{\pi}{2} U_1 \right)}
 ```
-and the first crossing after $1/2$, $W_{\tau_{\textrm{+}}}=0$ with $1/2<\tau_{+}<1$
+and the last crossing before $1/2$, $W_{\tau_{\textrm{-}}}=0$ with $0<\tau_{-}<1/2$
 ```math
-\tau_{\textrm{+}} = \frac{1-2\tau_{\textrm{-}} + \tau_{\textrm{-}} U_2^2}{1-2\tau_{\textrm{-}}+\;\;\;U_2^2}
+\tau_{\textrm{-}} = \frac{U_2^2\tau_{\textrm{+}} }{2\tau_{\textrm{+}}+U_2^2-1}
 ```
 with $U_1,U_2$ uniformly distributed over $(0,1)$.
--- src/test_moments.py
+++ src/test_moments.py
@@ -5,27 +5,27 @@ u1 = (np.arange(n) + 0.5) / n
 a = 0.4
 sin2 = np.square(np.sin(u1 * np.pi / 2))
 
-tm = a*sin2 / (1 - a + a*sin2)
-m1 = 1-(1-a)**0.5
-print('<t-> %.5f, numerically %.5f'%(m1, tm.mean()))
+tp = a / (a + (1-a)*sin2)
+tp_m1 = a**0.5
+print('<t+> %.5f, numerically %.5f'%(tp_m1, tp.mean()))
 
 n2 = n*2
 u2 = (np.arange(n2) + 0.5) / n2
 
-v = (1-a)*u2*u2
-tp = (a + np.multiply.outer(tm, v-1))/(a+np.add.outer(-tm, v))
-tp_m1 = a**0.5
-print('Average t+ %.5f, numerically %.5f'%(tp_m1, tp.mean()))
+v = a*u2*u2
+tm = (np.multiply.outer(tp, v))/(np.add.outer(tp, v)-a)
+tm_m1 = 1-(1-a)**0.5
+print('<t-> %.5f, numerically %.5f'%(tm_m1, tm.mean()))
 print(tm.shape, tp.shape)
-av_tp_given_tm_num = tp.mean(axis=1)
-av_tp_given_tm = tm - (tm-1)*np.sqrt((a-tm)/(1-a)) * np.arctan(np.sqrt((1-a)/(a-tm)))
-print('E[t+|t-]', av_tp_given_tm_num[0], av_tp_given_tm_num[n//2], av_tp_given_tm_num[-1])
-print('E[t+|t-]', av_tp_given_tm[0], av_tp_given_tm[n//2], av_tp_given_tm[-1])
-tmtp = (np.reshape(tm,(n,1)) * tp).mean(axis=1)
+av_tm_given_tp_num = tm.mean(axis=1)
+av_tm_given_tp = tp - tp*np.sqrt((tp-a)/a) * np.arctan(np.sqrt((a)/(tp-a)))
+print('E[t-|t+]', av_tm_given_tp_num[0], av_tm_given_tp_num[n//2], av_tm_given_tp_num[-1])
+print('E[t-|t+]', av_tm_given_tp[0], av_tm_given_tp[n//2], av_tm_given_tp[-1])
+tmtp = (np.reshape(tp,(n,1)) * tm).mean(axis=1)
 print(tmtp.shape)
 
 m2 = (a**1.5 - (a+2)*np.sqrt(1-a) + 2)/(3)
-print('<t-,t+> %.7f numerically %.7f'%(m2, np.inner(np.transpose(tp), tm).mean()/n), tmtp.mean(), (av_tp_given_tm*tm).mean())
+print('<t-,t+> %.7f numerically %.7f'%(m2, np.inner(np.transpose(tm), tp).mean()/n), tmtp.mean(), (av_tm_given_tp*tp).mean())
 print('Median t-', a/(2-a), np.median(tm))
 print('Median t+', 2*a/(1+a), np.median(tp.ravel()))
 
@@ -36,17 +36,17 @@ v = (1/np.pi)*(np.sqrt(s)*np.arcsin(np.sqrt((1-a)/(1-s))))
 print('sin integral', v.sum()*ds, (np.sqrt(1-a)+0.5*a*np.sqrt(1-a)+a**1.5-1)/3)
 print('scalar integral==<t-^2>', 1 - np.sqrt(1-a) * ((a+2)/2), (tm*tm).mean())
 
-var_tm = 0.5*np.sqrt(1-a)*m1*m1
-print('Variance <(t- - <t->)^2>', ((tm-m1)*(tm-m1)).mean(), var_tm)
+var_tm = 0.5*np.sqrt(1-a)*np.square(tm_m1)
+print('Variance <(t- - <t->)^2>', ((tm-tm_m1)*(tm-tm_m1)).mean(), var_tm)
 var_tp = 0.5*np.sqrt(a)*np.square(1-np.sqrt(a))
 print('Variance <(t+ - <t+>)^2>', np.square(tp-tp_m1).mean(), var_tp)
 
-covar = m2 - m1*tp_m1 # TODO simplify
-print('Covariance <(t+ - <t+>)(t- - <t->)>', covar, ((av_tp_given_tm - tp_m1)*(tm-m1)).mean())
+covar = m2 - tm_m1*tp_m1 # TODO simplify
+print('Covariance <(t+ - <t+>)(t- - <t->)>', covar, ((av_tm_given_tp - tm_m1)*(tp-tp_m1)).mean())
 
 print('Pearson correlation coefficient')
 correl = (2/3)*(1-a**0.5 - (1-a)**0.5)/((a*(1-a))**0.25)
-print('Correl <(t+ - <t+>)(t- - <t->)>/sqrt(<(t+ - <t+>)^2><(t- - <t->)^2>', correl, ((av_tp_given_tm - tp_m1)*(tm-m1)).mean()/np.sqrt(((tm-m1)*(tm-m1)).mean() *np.square(tp-tp_m1).mean()))
+print('Correl <(t+ - <t+>)(t- - <t->)>/sqrt(<(t+ - <t+>)^2><(t- - <t->)^2>', correl, ((av_tm_given_tp - tm_m1)*(tp-tp_m1)).mean()/np.sqrt(((tm-tm_m1)*(tm-tm_m1)).mean() *np.square(tp-tp_m1).mean()))
 
 
 print('Correl a=0.5, corr= (8**0.5 - 4)/3', (8**0.5-4)/3)
