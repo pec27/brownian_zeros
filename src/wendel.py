ade0a15a617e08806933514f5401886eae5723d2
Peter Creasey <peter@mercuna.com>
2025-02-06 17:17:13 +0000

    indefinite integral examples

--- src/wendel.py
+++ src/wendel.py
@@ -52,9 +52,37 @@ def test_M(n,a):
     print('Num int.', wendel_integral(a, 0, lambda x : M(n-1,x)))
     print('Analytic', M(n,a))
     
+def indef1(x,a):
+    # Wendel integral of the constant 1
+    return -1 + (2/pi)*(arcsin(sqrt(x/(1-a)))/sqrt(a) + arctan(sqrt((1-a-x)/(a*x))))
+
+def indef_inv_root(x,a):
+    # Wendel integral with 1/sqrt(a)
+    return 1/sqrt(a) - (2/pi)*(arcsin(sqrt(a/(1-x)))/sqrt(a) + sqrt(1-x-a)/a)
+def indef_root_func(x,a):
+    # Wendel integral with sqrt(a^(-2)-2/a)
+    
+    T1 = -sqrt((x-2*a)*(1-a-x)/a)
+    T2 = -2*sqrt(1-2*a)*arcsin(sqrt(a*(x-2*a)/((1-3*a)*(1-x))))
+    T3 = (1-a)*arcsin(sqrt((x-2*a)/(1-3*a)))/sqrt(a)
+
+    return (1/(pi* a))*(T1+T2 + T3)
+    
+def test_indef(a):
+
+    print('Test indefinite integrals')
+    print(wendel_integral(a,a,lambda x: 1))
+    print(indef1(1-a,a) - indef1(a,a))
+    print(wendel_integral(a,a,lambda x: 1/sqrt(x)))
+    print(indef_inv_root(1-a,a) - indef_inv_root(a,a))
+    print(wendel_integral(a,2*a,lambda x: sqrt(1-2*x)/x))
+    print(indef_root_func(1-a,a) - indef_root_func(2*a,a))
+    
 if __name__=='__main__':
     test_M(1,0.29)
     test_M(2,0.29)
     
     test_M(3,0.26)
     test_M(3,0.16)
+
+    test_indef(0.14)
